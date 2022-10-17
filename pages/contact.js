73bd031a2a88c710a9a2f449a0ee25702e59ee1c
Dominic Magnifico <dom@houseofgiants.com>
2022-10-17 12:44:26 -0600

    Update link to Imperial's website.

--- pages/contact.js
+++ pages/contact.js
@@ -13,7 +13,7 @@ const Contact = () => (
 				</div>
 
 				<div className="wrap">
-					<h2><a href="https://www.facebook.com/bristoltattooco/" target="_blank" rel="noopener noreferrer">Imperial Tattoo Company</a></h2>
+					<h2><a href="https://www.imperialtattoova.com/" target="_blank" rel="noopener noreferrer">Imperial Tattoo Company</a></h2>
 					<p><strong>Address:</strong> 793 W Main Street, Unit 7, Abingdon, VA 24210</p>
 					<p><strong>Phone:</strong> (276) 525-1140</p>
 					<iframe title="Google map location for Imperial Tattoo Company" style={{width: '100%'}} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3198.7268429813703!2d-82.00017888427392!3d36.705101379968276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x885012df8dca7a0f%3A0x58fd52c032a8d4e1!2sImperial%20Tattoo!5e0!3m2!1sen!2sus!4v1666032034750!5m2!1sen!2sus" height="450" frameBorder="0" allowFullScreen=""></iframe>
